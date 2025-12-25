89673654264135f46e8ca85741330d74409eb14f
akashsonawale <akashsonawale99@gmail.com>
2025-12-25 12:03:10 +0530

    https://github.com/akashsonawale/games.git

--- src/components/Spin the wheel/footer.js
+++ src/components/Spin the wheel/footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-icons">
-        <a href=""><i className="fab fa-github"></i></a>
-        <a href="#"><i className="fab fa-linkedin"></i></a>
-        <a href="#"><i className="fab fa-instagram"></i></a>
-        <a href="#"><i className="fab fa-twitter"></i></a>
+        <a href="https://github.com/akashsonawale"><i className="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/akash-sonawale-701042258/"><i className="fab fa-linkedin"></i></a>
+        <a href="https://www.instagram.com/akash13.12"><i className="fab fa-instagram"></i></a>
+        <a href="https://x.com/AkashSonaw31622"><i className="fab fa-twitter"></i></a>
       </div>
 
       <p className="footer-text">
